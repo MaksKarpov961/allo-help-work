4e9a847fc22ffe76471dc0deae4db6424dd23e38
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-07-22 19:55:03 +0300

    test version

---
+++ js/href-action.js
@@ -0,0 +1,18 @@
+function transliterate() {
+    const ukrInput = document.getElementById('ukrInput').value.toLowerCase();
+    const transliterationMap = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh',
+        'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts',
+        'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia', ' ': '-', "'": ''
+    };
+
+    let transliterated = '';
+    for (let char of ukrInput) {
+        transliterated += transliterationMap[char] !== undefined ? transliterationMap[char] : char;
+    }
+
+    document.getElementById('result').innerText = transliterated;
+}
+
+window.transliterate = transliterate;
